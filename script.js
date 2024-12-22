6b698a06726019bbcda05dd23afbffa2d07efa68
Deepak123455 <deepakmurugesh200@gmail.com>
2024-12-22 15:26:05 +0530

    Music Player

---
+++ script.js
@@ -0,0 +1,253 @@
+let songs = [
+    { 
+      SongTitle: "7 Aum Arivu - Mun Andhi", 
+      ArtistName: "Harris Jayaraj", 
+      src : "Songs/7 Aum Arivu - Mun Andhi Video _ Suriya_ Shruti _ Harris Jayaraj(M4A_128K).m4a",
+      img: "Images/download.jpeg",
+      time:"05:59"
+    },
+    { 
+      SongTitle: "Enakenna Yaarum Illaye", 
+      ArtistName: "Anirudh Ravichander", 
+      src:"Songs/Aakko - Enakenna Yaarum Illaye Lyric _ Anirudh Ravichander(M4A_128K).m4a", 
+      img: "Images/Enakena.jpeg",
+      time:"04:20"
+    },
+    { 
+      SongTitle: "We All need that", 
+      ArtistName: "Alone_ Pt.", 
+      src: "Songs/Alan Walker _ Ava Max - Alone_ Pt. II (Lyrics)(M4A_128K).m4a", 
+      img: "Images/Alan We.jpeg",
+      time:"02:57"
+    },
+    { 
+      SongTitle: "Angu Vaana Konilu (ARM)", 
+      ArtistName: "Vijayalakshmi", 
+      src:"Songs/Angu Vaana Konilu (Malayalam)_ARM_Tovino Thomas_Jithin Laal_Dhibu Ninan Thomas _Vaikom Vijayalakshmi(M4A_128K).m4a", 
+      img: "Images/ARM.jpeg",
+      time:"04:19"
+    },
+    { 
+      SongTitle: "Love Me Like You Do", 
+      ArtistName: "Ellie Goulding ", 
+      src:"Songs/Ellie Goulding - Love Me Like You Do (Lyrics)(M4A_128K).m4a", 
+      img: "Images/Love.jpeg",
+      time:"04:00"
+    },
+    { 
+      SongTitle: "Ethir Neechal", 
+      ArtistName: "Anirudh Ravichander", 
+      src:"Songs/Ethir Neechal(M4A_128K).m4a", 
+      img: "Images/Ethir.jpeg",
+      time:"04:31"
+    },
+    { 
+      SongTitle: "Hayyoda Lyrics- Jawan", 
+      ArtistName: "Anirudh", 
+      src:"Songs/Hayyoda Lyrics- Jawan _ Tamil Lyrics _ Anirudh _Priya Mali _(M4A_128K).m4a", 
+      img: "Images/hyoda.jpeg",
+      time:"03:10"
+    },
+    { 
+      SongTitle: "Hey Minnale", 
+      ArtistName: "GV Prakash", 
+      src : "Songs/Hey Minnale - Video Song _ Amaran _ Sivakarthikeyan_ Sai Pallavi _ GV Prakash _ Rajkumar Periasamy(M4A_128K).m4a", 
+      img: "Images/Amaran.jpeg",
+      time:"02:56"
+    },
+    { 
+      SongTitle: "Illuminati_Aavesham_Jithu", 
+      ArtistName: "Madhavan_Fahadh", 
+      src : "Songs/Illuminati_Aavesham_Jithu Madhavan_Fahadh Faasil_Sushin Shyam_Dabzee_Vinayak_ Nazriya_Anwar Rasheed(M4A_128K).m4a", 
+      img: "Images/Avesam.jpeg",
+      time:"02:28"
+    },
+    { 
+      SongTitle: "On The Floor", 
+      ArtistName: "Jennifer Lopez", 
+      src : "Songs/Jenefer Lopes - On The Floor (Lyrics_Letra)(M4A_128K).m4a", 
+      img: "Images/Onthefloor.jpeg",
+      time:"04:45"
+    },
+    { 
+      SongTitle: "Way Down We Go", 
+      ArtistName: "KALEO", 
+      src : "Songs/KALEO - Way Down We Go (Lyrics)(M4A_128K).m4a", 
+      img: "Images/Waydown.jpeg",
+      time:"03:40"
+    },
+    { 
+      SongTitle: "Manasilaayo", 
+      ArtistName: "Anirudh", 
+      src : "Songs/Manasilaayo Video _ Vettaiyan _ Rajinikanth _ Anirudh Ravichander _ Manju Warrier _ T.J. Gnanavel(M4A_128K).m4a", 
+      img: "Images/manasilaayo.jpeg",
+      time:"04:00"
+    },
+    { 
+      SongTitle: "Melliname", 
+      ArtistName: "Harish", 
+      src : "Songs/Melliname Song - Shajahan Tamil Movie _ Vijay _ Richa Pallod _ Harish Raghavendra _ Mani Sharma(M4A_128K).m4a", 
+      img: "Images/Melliname.jpeg",
+      time:"05:05"
+    },
+    { 
+      SongTitle: "Moral of the Story", 
+      ArtistName: "Ashe", 
+      src : "Songs/Moral of the Story - Ashe _ lyrics _ aesthetic _ WhatsApp status _ English songs _ speed up _ slowed(MP3_160K)_1.mp3", 
+      img: "Images/Moral.jpeg",
+      time:"00:15"
+    },
+    { 
+      SongTitle: "Unstoppable", 
+      ArtistName: "Sia", 
+      src : "Songs/Sia - Unstoppable (Lyrics)(M4A_128K).m4a", 
+      img: "Images/Unstoppable.jpeg",
+      time:"03:36"
+    },
+    { 
+      SongTitle: "Thani Oruvan", 
+      ArtistName: "Hiphop Tamizha", 
+      src : "Songs/Thani Oruvan - Theemai Dhaan Vellum Lyric _ Jayam Ravi_ Nayanthara _ Hiphop Tamizha(M4A_128K).m4a", 
+      img: "Images/Thani Oruvan.jpeg",
+      time:"03:33"
+    },
+    { 
+      SongTitle: "Nenjukkul Peidhidum", 
+      ArtistName: "Harris Jayaraj", 
+      src : "Songs/Vaaranam Aayiram - Nenjukkul Peidhidum Video _ Harris Jayaraj _ Suriya(M4A_128K).m4a", 
+      img: "Images/Vaaranam Aayiram.jpeg",
+      time:"05:39"
+    },
+    { 
+      SongTitle: "Naanga Vera Maari", 
+      ArtistName: "Yuvan Shankar", 
+      src : "Songs/Valimai - Naanga Vera Maari Video _ Ajith Kumar _ Yuvan Shankar Raja(M4A_128K).m4a", 
+      img: "Images/Valimai.jpeg",
+      time:"04:29"
+    },
+    { 
+      SongTitle: "Vennilavu Saaral Nee", 
+      ArtistName: "GV Prakash", 
+      src : "Songs/Vennilavu Saaral song (Lyric video) _ Amaran _ Sivakarthikeyan_ Sai Pallavi _ GV Prakash(M4A_128K).m4a", 
+      img: "Images/Vennilavu.jpeg",
+      time:"03:38"
+    },
+    { 
+      SongTitle: "Hunter Vantaar", 
+      ArtistName: "Anirudh", 
+      src : "Songs/Vettaiyan - Hunter Vantaar Lyric Video _ Rajinikanth _ Anirudh Ravichander _ T.J.Gnanavel(M4A_128K).m4a", 
+      img: "Images/Vettaiyan.jpeg",
+      time:"03:19"
+    },
+    { 
+      SongTitle: "Varayo Varayo Kadhal", 
+      ArtistName: "Harris Jayarai", 
+      src : "Songs/ஆதவன் - வாராயோ வாராயோ தமிழ் பாடல்வரிகள் _ சூர்யா(M4A_128K).m4a", 
+      img: "Images/aathavan.jpeg",
+      time:"04:40"
+    }
+  ];
+  const audio = document.createElement("audio")
+  const play = document.querySelector("#play")
+  const artist = document.getElementById("Artist")
+  const img = document.querySelector("#img")
+  const slider = document.getElementById("range")
+  const title = document.getElementById("title")
+  const timer = document.getElementById("timer")
+  const timer1 = document.getElementById("timer1")
+  const SongDuration = document.getElementById("S_Duration")
+  let index = 0
+  let play_Song = false
+
+  function Loadtrack(index){
+    audio.src = songs[index].src
+    title.innerText = songs[index].SongTitle
+    img.src = songs[index].img
+    artist.innerText = songs[index].ArtistName
+    SongDuration.innerText = songs[index].time
+    // audio.load()
+    audio.play()
+  }
+
+  Loadtrack(index)
+
+  function Play(){
+    if(play_Song == false) playSong()
+    else pauseSong()
+  }
+
+  function playSong(){
+     audio.load()
+    audio.play()
+    play_Song = true 
+    play.innerHTML = `<i class="fa-solid fa-pause text-white text-lg"></i>`
+  }
+  function pauseSong(){
+    audio.pause()
+    play_Song = false
+    play.innerHTML = `<i class="fa-solid fa-play text-white text-lg"></i>`
+  }
+
+  function Next(){
+      if (index < songs.length - 1) index += 1; 
+      else index = 0; 
+      Loadtrack(index); 
+      slider.value = 0; 
+      if (play_Song) playSong(); 
+      else play.innerHTML = `<i class="fa-solid fa-play text-white text-lg"></i>`;
+    }
+  
+  function Prv(){
+    if (index > 0) index -= 1;
+    else index = songs.length - 1; 
+    Loadtrack(index); 
+    if (play_Song) playSong(); 
+    else play.innerHTML = `<i class="fa-solid fa-play text-white text-lg"></i>`; 
+  }
+
+  slider.addEventListener("input", () => {
+    audio.addEventListener("timeupdate", () => {
+      if (audio.duration) {
+        const progress = (audio.currentTime / audio.duration) * 100;
+        slider.value = progress; 
+        const currentTimeInMinutes = Math.floor(audio.currentTime / 60);
+        const currentTimeInSeconds = Math.floor(audio.currentTime % 60);
+        timer.innerText = currentTimeInMinutes < 10 ? `0${currentTimeInMinutes}` : currentTimeInMinutes;
+        timer1.innerText = currentTimeInSeconds < 10 ? `0${currentTimeInSeconds}` : currentTimeInSeconds;
+      }
+    });
+    const value = slider.value;
+  const newTime = (value / 100) * audio.duration; 
+  audio.currentTime = newTime;
+    if (audio.readyState > 0) { 
+      const slidePosition = audio.duration * (slider.value / 100);
+      audio.currentTime = slidePosition;
+    } else {
+      console.error("Audio metadata not loaded yet.");
+    }
+  });
+
+  function Auto() {
+    
+    index++;
+    if (index >= songs.length) {
+      index = 0; 
+    }
+    Loadtrack(index);
+  }
+  audio.addEventListener("ended", () => {
+    Auto();
+  });
+  
+ 
+  Loadtrack(index);
+  
+
+
+
+
+
+ 
+ 
+
+  
